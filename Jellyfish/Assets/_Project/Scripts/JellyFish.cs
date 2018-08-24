62fc97c5bf153f6dd113ad1d1c478c41ba5f59e7
Rudy <ruddycarpio@gmail.com>
2018-08-24 12:44:30 +1000

    Wave movement added

--- Jellyfish/Assets/_Project/Scripts/JellyFish.cs
+++ Jellyfish/Assets/_Project/Scripts/JellyFish.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class JellyFish : MonoBehaviour, IPointerClickHandler
+public class JellyFish : MonoBehaviour
 {
     public GameObject scene;
     public Renderer m_MeshRenderer;
     private Material m_Material;
     public static Transform mSelectedSpawnPoint;
+    private Collider _collider;
     private Camera cam;
     void Awake()
     {
         m_Material = m_MeshRenderer.material;
         //Set the main spawn point
         cam = Camera.main;
+        _collider = GetComponent<Collider>();
     }
 
     void Update()
@@ -23,7 +25,7 @@ public class JellyFish : MonoBehaviour, IPointerClickHandler
     }
 
     //Pointer or Gaze clicked the jelly fish
-    public void OnPointerClick(PointerEventData eventData)
+    public void OnTriggerEnter(Collider collider)
     {
         AppManager.Instance.OnJellyFishCreated(this, transform.position, transform.rotation,scene.transform);
     }
